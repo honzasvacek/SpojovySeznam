21e2d2e7d2b654e8a7f8da9afdfc3548ea18ac6a
Jan Sváček <jan@svacek.eu>
2023-05-21 10:27:06 +0200

    Fixed error double save at the start.

--- src/Main.java
+++ src/Main.java
@@ -8,27 +8,31 @@ public class Main {
 
         int hodnota = sc.nextInt();
 
-        Prvek hlava = new Prvek(hodnota, null);
+        Prvek tail = new Prvek(hodnota, null); //první hodnota je zároveň i tail = konec
+        
+        Prvek head = tail; //uložím si první prvek = head
 
-        Prvek start = hlava;
+        boolean cti = true;
 
-        while(hodnota != 0) {
+        while(cti) {
             //čtu dokud nenarazím na nulu a vytvářím prvky
-            Prvek novyPrvek = new Prvek(hodnota, null);
-
-            hlava.dalsiPrvek = novyPrvek;
-
-            hlava = novyPrvek;
-
             hodnota = sc.nextInt();
+            if (hodnota == 0) {
+                cti = false;
+            } else {
+                Prvek novyPrvek = new Prvek(hodnota, null); //nový prvek zatím nenavazuje nikoho
+
+                tail.dalsiPrvek = novyPrvek; //na tail navážu nový prvek
+
+                tail = novyPrvek; //tail je teď ten nový prvek
+            }
         }
-        System.out.println("Spojovy seznam:");
+        System.out.println("Spojovy seznam: ");
 
         //výpis seznamu
-        while(start.dalsiPrvek != null) {
-            start = start.dalsiPrvek;
-
-            System.out.println(start.hodnota);
+        while(head != null) {
+            System.out.println(head.hodnota);
+            head = head.dalsiPrvek;
         }
 
     }
